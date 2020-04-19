37582815cdc542f74fe7f02db39dacf3be6cf86e
July Angelica Moreno Jara <july.moreno@globant.com>
2020-04-19 12:08:59 -0500

    Globant Structure - Initial commit

---
+++ GlobantStructure/src/com/java/training/interfaces/AssignationsQcTae.java
@@ -0,0 +1,10 @@
+package com.java.training.interfaces;
+
+public interface AssignationsQcTae {
+
+	/** Common Methods between QC Analyst and TAE Analyst **/
+	
+	public abstract String defectWriteUp();
+	public String closeDefects();
+
+}
